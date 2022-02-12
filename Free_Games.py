a2870a55705aaa6ad07fd3da54d339b6af69c8b5
Yeshey <a2020144466@isec.pt>
2022-02-12 02:18:47 +0000

    made a web page to check if it is fullscreen

--- Free_Games.py
+++ Free_Games.py
@@ -7,6 +7,7 @@ import sys
 import subprocess
 import pyperclip
 import lxml.html, pyotp, re, sys, time, traceback
+import tempfile
 from datetime import datetime
 
 from os import getenv
@@ -40,16 +41,34 @@ def open_browser(url, whichBrowser=Empty):
         else:
             os.system("firefox --private-window " + url)
     '''
+    isfullscreanurl = 'data:text/html,<title>FullScreen?</title><body><div class="answer"></div></body><style>body{background-color:white !important;} .answer{position: absolute;top: 50%;left: 50%;transform: translate(-50%, -50%);height: 90vh;width: 90vw;display:grid;place-items: center;font-size: min(80vh,40vw);font-family:"Segoe UI", Tahoma, Geneva, Verdana, sans-serif;}</style><script>function updt() { if((window.fullScreen) || (window.innerWidth == screen.width && window.innerHeight == screen.height)) {str = "YES"} else {str = "NO"} document.querySelector(".answer").innerHTML=str } window.addEventListener("resize", () => { updt() }); updt()</script>'
+
+    #https://www.kite.com/python/answers/how-to-write-to-an-html-file-in-python
+    html = '<html> ...  generated html string ...</html>'
+    with tempfile.NamedTemporaryFile('w', delete=False, suffix='.html') as f:
+        url = 'file://' + f.name
+        f.write(html)
+    webbrowser.open(url)
+
+    exit()
+
     if (whichBrowser == Empty):
-        webbrowser.get().open(url)
+        webbrowser.get().open(isfullscreanurl)
     else:
-        webbrowser.get(whichBrowser).open(url)
+        webbrowser.get(whichBrowser).open(isfullscreanurl)
+
+    
 
     #pyautogui.hotkey('ctrl', 'shift', 'n')
-    time.sleep(2)
-    pyautogui.press('F11')
+    img = wait_to_see('NO.png', True, 20, 'YES.png')
+    if img is None:
+        pyautogui.press('F11')
+        img = wait_to_see('NO.png', True, 20, 'YES.png')
+    img = pyautogui.locateCenterOnScreen('imgs/NO.png', grayscale=True, confidence=.7)
+    if img is not None:
+        pyautogui.press('F11')
 
-def wait_to_see(rec_img, moveMouse = True, timeout=15):
+def wait_to_see(rec_img, moveMouse = True, timeout=20, rec_img2=Empty):
     if (moveMouse == True):
         pyautogui.moveTo(1,1)
     print("looking for",rec_img, end="")
@@ -63,13 +82,21 @@ def wait_to_see(rec_img, moveMouse = True, timeout=15):
         img = pyautogui.locateCenterOnScreen('imgs/'+rec_img, grayscale=True, confidence=.7)
         if img is not None:
             break 
+        if (rec_img2 != Empty): 
+            img = pyautogui.locateCenterOnScreen('imgs/'+rec_img2, grayscale=True, confidence=.7)
+            if img is not None:
+                break 
     return img
 
 def go_to_url(url):
+    #pyautogui.hotkey('ctrl', 'l')
+    #for letter in url:    
+    #    pyperclip.copy(letter)
+    #    pyautogui.hotkey('ctrl', 'v')
+    #pyautogui.press ('enter')
     pyautogui.hotkey('ctrl', 'l')
-    for letter in url:    
-        pyperclip.copy(letter)
-        pyautogui.hotkey('ctrl', 'v')
+    pyperclip.copy(url)
+    pyautogui.hotkey('ctrl', 'v')
     pyautogui.press ('enter')
 
 def log_into_account(email, password, two_fa_key=None):
@@ -80,13 +107,6 @@ def log_into_account(email, password, two_fa_key=None):
     img = pyautogui.locateCenterOnScreen('imgs/LogedIn.png', grayscale=True, confidence=.7) 
     if (img is not None):
         go_to_url(epic_logout_url)
-        '''
-        pyautogui.moveTo(img)
-        img = wait_to_see('SignOut.png', False)
-        pyautogui.click(img)
-        time.sleep(2)
-        wait_to_see('LanguageGlobe.png')
-        '''
     else:
         img = pyautogui.locateCenterOnScreen('imgs/signIn.png', grayscale=True, confidence=.7)
         if img is not None:
@@ -98,12 +118,9 @@ def log_into_account(email, password, two_fa_key=None):
     img = wait_to_see("EmailAddress.png")
     pyautogui.click(img)
 
-    for letter in email:
-        if letter == '@':
-            pyperclip.copy('@')
-            pyautogui.hotkey('ctrl', 'v')
-        else:
-            pyautogui.write(letter)
+    pyperclip.copy(email)
+    pyautogui.hotkey('ctrl', 'v')
+
     pyautogui.press('Tab')
     pyautogui.write(password)
     for i in range(0, 4):
@@ -129,10 +146,17 @@ def claim_free_games():
         img = pyautogui.locateCenterOnScreen('imgs/FREENOW.png', grayscale=True, confidence=.7)
         if img is not None:
             break  
-        #img = pyautogui.locateCenterOnScreen('imgs/present.png', grayscale=True, confidence=.7)
-        #if img is not None:
-        #    break
     pyautogui.click(img)
+
+    # wait to see in_library.png or get.png
+    img = wait_to_see('get.png', True, 20, 'IN_LIBRARY.png')
+
+    img = pyautogui.locateCenterOnScreen('imgs/get.png', grayscale=True, confidence=.7) 
+    if img is not None:
+        pyautogui.click(img)
+        wait_to_see('PLACE_ORDER.png')
+    else:
+        return
 
 
 if __name__ == '__main__':
